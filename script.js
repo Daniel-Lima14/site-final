e693e154e04ba427ca8c31ead888e141170412bb
Daniel-Lima14 <91990139+Daniel-Lima14@users.noreply.github.com>
2021-12-10 21:34:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let pergunta = prompt("em que ano o galo foi fundado")
+let resposta = 1908 
+if(pergunta == resposta){
+    pergunta = pergunta == resposta
+    alert(`resposta correta  , a resposta e  ${resposta}`)
+}
+
+
+else {alert("resposta errada")
+}
+
+let pergunta1 = prompt("quantos titulos brasileiros o Atletico Mineiro tem")
+let resposta1 = 2 
+if(pergunta1 == resposta1){
+    pergunta1 = pergunta1 == resposta1
+    alert(`resposta correta  , a resposta e  ${resposta1}`)
+}
+
+
+else {alert("resposta errada")
+}
+
+let pergunta2 = prompt("em que ano o Atletico Mineiro ganhou a libertadores")
+let resposta2 = 2013 
+if(pergunta2 == resposta2){
+    pergunta2 = pergunta2 == resposta2
+    alert(`resposta correta  , a resposta e  ${resposta2}`)
+}
+
+
+else {alert("resposta errada")
+}
+
+let pergunta3 = prompt("em que ano o Atletico Mineiro ganhou a Copa do Brasil")
+let resposta3 = 2014 
+if(pergunta3 == resposta3){
+    pergunta3 = pergunta3 == resposta3
+    alert(`resposta correta  , a resposta e  ${resposta3}`)
+}
+
+
+else {alert("resposta errada")
+}
+
+let pergunta4 = prompt("em que ano o Atletico mineiro conquistou seu primeiro titulo Brasileiro")
+let resposta4 = 1971 
+if(pergunta4 == resposta4){
+    pergunta4 = pergunta4 == resposta4
+    alert(`resposta correta  , a resposta e  ${resposta4}`)
+}
+
+
+else {alert("resposta errada")
+}
